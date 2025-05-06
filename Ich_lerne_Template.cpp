4faacdc8a8274ff6bae5703af347156786fc39b2
djoufoalainmarcel@gmail.com <djoufoalainmarcel@gmail.com>
2025-05-06 10:28:15 +0200

    wie benutzt man template

---
+++ Ich_lerne_Template.cpp
@@ -0,0 +1,21 @@
+#include <iostream >
+#include <math.h>
+using namespace std ; 
+template < typename T >
+T addition (T a ,  T b ){
+    return a + b ;
+}
+int main (){
+    cout << addition (3 ,5 ) << endl ; 
+    cout << addition (4.5 ,6.7 )<< endl ; 
+    string name {"alain "}; 
+    string Vorname {"Marcel "}; 
+    cout << addition (name , Vorname); 
+    char v {'b'}; 
+    char c {'a'};
+    cout << addition ( v , c ); 
+
+    //cout << addition (  Alain, Marcel )<< endl ; 
+    return 0;
+    
+}
